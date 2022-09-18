8e85611ef49a06695729147a2a134d8c7d6e6ded
hyena0608 <goatsfish@gmail.com>
2022-09-19 07:00:08 +0900

    remove : 필요 없는 인터페이스 삭제

--- src/main/java/domain/EnumCalculator.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-import java.util.function.BiFunction;
-
-public interface EnumCalculator {
-
-    BiFunction<Integer, String, Integer> calculate(Integer num1, String type, Integer num2);
-}
