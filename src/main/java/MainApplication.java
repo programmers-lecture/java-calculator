93d136f9785d8848b47df0febd802748a5a43e21
hyena0608 <goatsfish@gmail.com>
2022-09-19 19:08:22 +0900

    feat : 결과 출력 기능 구현

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,6 +1,7 @@
 import calculator.Calculator;
 import converter.FormulaConverter;
 import view.Reader;
+import view.Sender;
 
 import java.util.List;
 // 12412 - 1348 + 134 * 10 - (10 / 10 * 20 + 10 - 4) + 10 - 20 / 2
@@ -9,6 +10,6 @@ public class MainApplication {
     public static void main(String[] args) {
         List<String> formula = new FormulaConverter().getFormula(Reader.read());
         Integer result = new Calculator().getResult(formula);
-        System.out.println("result = " + result);
+        Sender.sendCalResultMessage(result.toString());
     }
 }
---
+++ src/main/java/view/Sender.java
@@ -0,0 +1,8 @@
+package view;
+
+public class Sender {
+
+    public static void sendCalResultMessage(String message) {
+        System.out.println("결과 = " + message);
+    }
+}
