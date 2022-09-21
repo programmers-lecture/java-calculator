26c2a55ee33d0cebeb2914771463fc8f13ef293a
JEONG-JIWHAN <jjw981210@gmail.com>
2022-09-21 16:44:46 +0900

    Enum 수정

--- src/main/java/calculator/domain/Operator.java
+++ src/main/java/calculator/domain/Operator.java
@@ -1,78 +1,43 @@
 package calculator.domain;
 
-public enum Operator {
-    PLUS("+",1){
-        @Override
-        public double apply(final double operand1, final double operand2){
-            return operand1+operand2;
-        }
-    },
-    MINUS("-",1){
-        @Override
-        public double apply(final double operand1, final double operand2){
-            return operand1-operand2;
-        }
-    },
-    MUL("*",2){
-        @Override
-        public double apply(final double operand1, final double operand2){
-            return operand1*operand2;
-        }
-    },
-    DIV("/",2){
-        @Override
-        public double apply(final double operand1, final double operand2){
-            if(operand2==0){
-                throw new IllegalArgumentException("0으로 나눌수 없습니다.");
-            }
-            return operand1/operand2;
-        }
-    };
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.BiFunction;
 
-    private final String textOperator;
+public enum Operator {
+    PLUS("+",1, (operand1,operand2) -> operand1+operand2),
+    MINUS("-",1, (operand1,operand2) -> operand1-operand2),
+    MUL("*",2, (operand1,operand2) -> operand1*operand2),
+    DIV("/",2, (operand1,operand2) -> {
+      if(operand2 == 0){
+        throw new IllegalArgumentException("0으로 나눌수 없습니다");
+      }
+      return operand1/operand2;
+  });
+
+    private final String symbol;
     private final int priority;
 
-    Operator(String textOperator, int priority){
-        this.textOperator = textOperator;
+    private final BiFunction<Double, Double, Double> operation;
+
+    Operator(String symbol, int priority, BiFunction<Double, Double, Double> operation){
+        this.symbol = symbol;
         this.priority = priority;
+        this.operation = operation;
     }
 
-    public String getTextOperator() {
-        return textOperator;
+    public String getSymbol() {
+        return symbol;
     }
 
     public int getPriority() {
-        return priority;
-    }
-
-    public static Operator findOperator(String op) {
-
-        if (PLUS.getTextOperator().equals(op)) {
-            return PLUS;
-        }
-        if (MINUS.getTextOperator().equals(op)) {
-            return MINUS;
-        }
-        if (MUL.getTextOperator().equals(op)) {
-            return MUL;
-        }
-        return DIV;
-    }
-
-    public static int findPriority(String op) {
-
-        if (PLUS.getTextOperator().equals(op)) {
-            return PLUS.getPriority();
-        }
-        if (MINUS.getTextOperator().equals(op)) {
-            return MINUS.getPriority();
-        }
-        if (MUL.getTextOperator().equals(op)) {
-            return MUL.getPriority();
-        }
-        return DIV.getPriority();
+      return priority;
     }
 
 
-    public abstract double apply(double operand1, double operand2);
+    public double operate(double operand1, double operand2){
+      return operation.apply(operand1, operand2);
+    }
+
+
 }
