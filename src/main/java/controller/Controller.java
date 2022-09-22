e3807fcd107af312b20f9678c5c265c5bdcb820a
preferkim <shineecard5@gmail.com>
2022-09-23 01:47:47 +0900

    refactor: OutputView 종료 메시지 출력 메서드 수정, static 제거

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,13 +1,24 @@
 package controller;
 
-import InputView
+import util.Reader;
+import view.InputView;
+import view.OutputView;
 
 public class Controller {
     private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
 
     public void run() {
+        String expression = Reader.read();
 
-        while (true) {
+        while (!isExitOrder(expression)) {
+            System.out.println("test");
         }
+
+        outputView.
+    }
+
+    private boolean isExitOrder(String order) {
+        return order.equals("0");
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,13 +1,13 @@
 package view;
 
 public class OutputView {
-    static final String EXIT_MESSAGE = "프로그램을 종료합니다...";
+    private static final String EXIT_MESSAGE = "프로그램을 종료합니다...";
 
-    public static String exit() {
-        return EXIT_MESSAGE;
+    public void printExitMessage() {
+        System.out.println(EXIT_MESSAGE);
     }
 
-    public static void print(Integer result) {
+    public void printCalculationResult(Integer result) {
         System.out.printf("결과는 %d\n", result);
     }
 }
