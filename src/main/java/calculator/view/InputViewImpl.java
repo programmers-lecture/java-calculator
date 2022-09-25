92e5da57d1d2fe089c188d2795c8854303bac321
K-jun98 <alap_u@naver.com>
2022-09-25 18:35:42 +0900

    refactor - 컨벤션 적용

--- src/main/java/calculator/view/InputViewImpl.java
+++ src/main/java/calculator/view/InputViewImpl.java
@@ -2,7 +2,7 @@ package calculator.view;
 
 import java.util.Scanner;
 
-public class InputViewImpl implements InputView{
+public class InputViewImpl implements InputView {
 
     private static Scanner scanner = new Scanner(System.in);
 
