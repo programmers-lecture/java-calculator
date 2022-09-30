92f0a1c105a835162d084493a0de590338585d14
joona95 <joona95@naver.com>
2022-10-01 07:46:03 +0900

    refactor: Util 숫자 여부 판단 성공, 실패 테스트 분리하여 리팩토링

--- src/test/java/caculator/utils/UtilTest.java
+++ src/test/java/caculator/utils/UtilTest.java
@@ -1,18 +1,26 @@
 package caculator.utils;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class UtilTest {
 
-    @Test
-    @DisplayName("숫자인지 여부 판단 테스트")
-    public void isNumberTest() {
-        assertThat(Util.isNumber("1"))
-                .isTrue();
-        assertThat(Util.isNumber("숫자아님"))
-                .isFalse();
+    @ParameterizedTest(name = "strings: 숫자에 해당되는 글자")
+    @DisplayName("숫자인지 여부 판단 성공 테스트")
+    @ValueSource(strings = {"1", "2", "10", "5.5"})
+    public void isNumberSuccessTest(String number) {
+        boolean actual = Util.isNumber(number);
+        assertThat(actual).isTrue();
+    }
+
+    @ParameterizedTest(name = "strings: 숫자에 해당되지 않는 글자")
+    @DisplayName("숫자인지 여부 판단 실패 테스트")
+    @ValueSource(strings = {"(", "+", "숫자아님"})
+    public void isNumberFailTest(String notNumber) {
+        boolean actual = Util.isNumber(notNumber);
+        assertThat(actual).isFalse();
     }
 }
