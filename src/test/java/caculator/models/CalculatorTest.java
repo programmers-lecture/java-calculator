f14827f3d47fa8bff65d5a5b6c6a4a2ece631f98
joona95 <joona95@naver.com>
2022-10-01 07:57:04 +0900

    refactor: 필요없는 import 제거

--- src/test/java/caculator/models/CalculatorTest.java
+++ src/test/java/caculator/models/CalculatorTest.java
@@ -1,13 +1,10 @@
 package caculator.models;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
