90676a3bc08c73e7cc413a2b9fa1c97c33f26f4e
hyena0608 <goatsfish@gmail.com>
2022-10-02 13:22:45 +0900

    후위 표기 변환 테스트 추가

--- src/test/java/stringcalculator/converter/PostfixConverterTest.java
+++ src/test/java/stringcalculator/converter/PostfixConverterTest.java
@@ -1,27 +1,44 @@
-//package stringcalculator.converter;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.ValueSource;
-//import stringcalculator.view.Reader;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//class PostfixConverterTest {
-//
-//    private final PostfixConverter converter = new PostfixConverter();
-//
-//    @Test
-//    @DisplayName("후위 표기식 작성 메서드 테스트")
-//    void postfixFormatTest() {
-//
-//        String str = " 1-4+2+(3*2+(1+1)-1)";
-//        List<String> format = converter.getFormula(Reader.splitWithoutSpace(str));
-//
-//        assertEquals("[1, 4, -, 2, +, 3, 2, *, 1, 1, +, +, 1, -, +]", format.toString());
-//    }
-//
-//}
+package stringcalculator.converter;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static stringcalculator.view.Reader.splitWithoutSpace;
+
+class PostfixConverterTest {
+
+    private final PostfixConverter converter = new PostfixConverter();
+
+    @ParameterizedTest(name = "[{index}] 연산식 = {0}, 변환 결과 = {1}")
+    @MethodSource("getFormulaToTestWhenConvertToPostfixThenSuccess")
+    @DisplayName("후위 표기식 변환 성공 테스트")
+    void postfixFormatTest(String formula, String result) {
+        List<String> convertedResult = converter.getFormula(splitWithoutSpace(formula));
+        assertThat(convertedResult.toString()).isEqualTo(result);
+    }
+
+    static Stream<Arguments> getFormulaToTestWhenConvertToPostfixThenSuccess() {
+        return Stream.of(
+                Arguments.arguments("1 + 2", "[1, 2, +]"),
+                Arguments.arguments("1 - 2", "[1, 2, -]"),
+                Arguments.arguments("1 * 2", "[1, 2, *]"),
+                Arguments.arguments("1 / 2", "[1, 2, /]"),
+                Arguments.arguments("1 + 2 + 3", "[1, 2, +, 3, +]"),
+                Arguments.arguments("1 + 2 * 3", "[1, 2, 3, *, +]"),
+                Arguments.arguments("1 * 2 + 3", "[1, 2, *, 3, +]"),
+                Arguments.arguments("1 * 2 / 3", "[1, 2, *, 3, /]"),
+                Arguments.arguments("1 / 2 * 3", "[1, 2, /, 3, *]"),
+                Arguments.arguments(
+                        "1 - 4 + 2 + ( 3 * 2 + ( 1 + 1 ) - 1 )",
+                        "[1, 4, -, 2, +, 3, 2, *, 1, 1, +, +, 1, -, +]"
+                )
+        );
+    }
+
+}
