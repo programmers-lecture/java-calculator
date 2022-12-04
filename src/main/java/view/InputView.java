f61979c32c92639b0989a9b0856d7eb7b2717949
Taehyun Jung <jstm1207@gmail.com>
2022-12-04 21:04:21 +0900

    feat : 문자열 계산기 필수 요구사항 구현 (#12)

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,10 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static String read() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+}
