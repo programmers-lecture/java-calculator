beef7da6358c1002ad7c95d2c8f244eb35ffc421
kssumin <201566@jnu.ac.kr>
2022-12-04 21:47:57 +0900

    feat(Operator) : 연산 구현

---
+++ src/main/java/Operator.java
@@ -0,0 +1,33 @@
+public enum Operator{
+    PLUE("+"){
+        @Override
+        public Integer calculate(Integer a, Integer b) {
+            return a+b;
+        }
+    },
+    MINUE("-") {
+        @Override
+        public Integer calculate(Integer a, Integer b) {
+            return a-b;
+        }
+    },
+    DIVIDE("/"){
+        @Override
+        public Integer calculate(Integer a, Integer b) {
+            return return a/b;
+        }
+    },
+    MULTIPLE("*"){
+        @Override
+        public Integer calculate(Integer a, Integer b) {
+            return a*b;
+        }
+    };
+    
+    private String operation;
+    
+    Operator(String operation){
+        this.operation=operation;
+    }
+    public abstract Integer calculate(Integer a, Integer b);
+}
