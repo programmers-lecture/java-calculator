5c7c5464648131f886417dbf478a2cd2f5d12515
namgonkim <32921225+namgonkim@users.noreply.github.com>
2022-12-05 14:04:25 +0900

    [feat] 소수 적용

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,17 @@
 public class Calculator {
     public double getResult(String[] formula) {
-        double result = Integer.parseInt(formula[0]);
+        double result = Double.parseDouble(formula[0]);
 
         for(int i=1; i<formula.length; i+=2) {
             String op = formula[i];
             if(op.equals("+")) {
-                result += Integer.parseInt(formula[i+1]);
+                result += Double.parseDouble(formula[i+1]);
             } else if(op.equals("-")) {
-                result -= Integer.parseInt(formula[i+1]);
+                result -= Double.parseDouble(formula[i+1]);
             } else if(op.equals("*")) {
-                result *= Integer.parseInt(formula[i+1]);
+                result *= Double.parseDouble(formula[i+1]);
             } else if(op.equals("/")) {
-                result /= Integer.parseInt(formula[i+1]);
+                result /= Double.parseDouble(formula[i+1]);
             }
         }
         return result;
