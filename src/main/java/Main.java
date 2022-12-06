2a449282d40aff21a4244529208b51b957450666
s-ryuri <fbfbf1@naver.com>
2022-12-06 23:01:32 +0900

    Refactor : 기존 데큐 및 리스트 전역 변수로 변경 후 계산 로직 메서드 추출

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 
 public class Main {
 
+    static Deque<Integer> number = new ArrayDeque<>();
+    static Deque<String> symbol = new ArrayDeque<>();
+    static List<String> symbols = Arrays.asList("+", "-", "*", "/");
+
     public static void main(String[] args) {
         final String[] formula = getFolmula();
-
-        Deque<Integer> number = new ArrayDeque<>();
-        Deque<String> symbol = new ArrayDeque<>();
-        List<String> symbols = Arrays.asList("+", "-", "*", "/");
 
         for (int i = 0; i < formula.length; i++) {
             char ch = getCh(formula[i]);
@@ -20,29 +20,12 @@ public class Main {
                     number.add((int) ch - 48);
                 }
                 if (number.size() == 1) {
-                    //이미 숫자가 한 개 들어가 있으면
-                    //심볼에 문자가 있는 지 확인하고
-                    //있으면 계산한다.
                     if (symbol.size() == 1) {
                         final String popSymbol = symbol.pop();
                         final Integer firstNumber = number.pollFirst();
                         final Integer secondNumber = (int) ch - 48;
 
-                        if (popSymbol.equals("+")) {
-                            number.addFirst(firstNumber + secondNumber);
-                        }
-
-                        if (popSymbol.equals("*")) {
-                            number.add(firstNumber * secondNumber);
-                        }
-
-                        if (popSymbol.equals("-")) {
-                            number.add(firstNumber - secondNumber);
-                        }
-
-                        if (popSymbol.equals("/")) {;
-                            number.add(firstNumber / secondNumber);
-                        }
+                        calculate(popSymbol, firstNumber, secondNumber);
                     }
                 }
             }
@@ -54,6 +37,26 @@ public class Main {
         }
 
         System.out.println("계산 결과 : " + number.pollFirst());
+    }
+
+    private static void calculate(final String popSymbol,
+                                  final Integer firstNumber,
+                                  final Integer secondNumber) {
+        if (popSymbol.equals("+")) {
+            number.addFirst(firstNumber + secondNumber);
+        }
+
+        if (popSymbol.equals("*")) {
+            number.add(firstNumber * secondNumber);
+        }
+
+        if (popSymbol.equals("-")) {
+            number.add(firstNumber - secondNumber);
+        }
+
+        if (popSymbol.equals("/")) {
+            number.add(firstNumber / secondNumber);
+        }
     }
 
     private static boolean isNumber(final char ch) {
