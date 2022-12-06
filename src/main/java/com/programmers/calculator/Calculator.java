ab988ce0c770e0391b563328bbd9d211ba7aa822
minkyeng <jomk0609@inzent.com>
2022-12-06 14:54:54 +0900

    refactor : 값을 입력받아 사칙연산 후, 결과 출력 (삼항이상)

--- src/main/java/com/programmers/calculator/Calculator.java
+++ src/main/java/com/programmers/calculator/Calculator.java
@@ -4,27 +4,27 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        int result = 0;
-
 //      연산식 입력받기
         Scanner scanner = new Scanner(System.in);
         String[] expressions = scanner.nextLine().split(" ");
 
-//      입력 받은 데이터로 연산하기(이항)
-//      TODO. 삼항이상의 연산
+//      연산 결과값 변수 초기화
+        int result = Integer.parseInt(expressions[0]);
+
+//      입력 받은 데이터로 사칙연산하기
         for (int i = 0; i < expressions.length; i++) {
             if (expressions[i].equals("+")) {
-                result = Integer.parseInt(expressions[i - 1]) + Integer.parseInt(expressions[i + 1]);
+                result += Integer.parseInt(expressions[i + 1]);
             } else if (expressions[i].equals("-")) {
-                result = Integer.parseInt(expressions[i - 1]) - Integer.parseInt(expressions[i + 1]);
+                result -= Integer.parseInt(expressions[i + 1]);
             } else if (expressions[i].equals("*")) {
-                result = Integer.parseInt(expressions[i - 1]) * Integer.parseInt(expressions[i + 1]);
+                result *= Integer.parseInt(expressions[i + 1]);
             } else if (expressions[i].equals("/")) {
-                result = Integer.parseInt(expressions[i - 1]) / Integer.parseInt(expressions[i + 1]);
+                result /= Integer.parseInt(expressions[i + 1]);
             }
         }
 
-//        연산 결과 출력
+//      연산 결과 출력
         System.out.println("결과 : " + result);
     }
 }
