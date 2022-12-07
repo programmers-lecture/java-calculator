c17e4f6df1d0eda3b440232af17f2343a65b3ac5
hong-kyungsik <h.ks.dpr@gmail.com>
2022-12-07 22:07:11 +0900

    Refactor: SentenceParser 에서 자료형과 무관한 부분 로직 분리

--- src/main/java/calculator/SentenceParser.java
+++ src/main/java/calculator/SentenceParser.java
@@ -1,5 +1,39 @@
 package calculator;
 
-public interface SentenceParser<T> {
-    Operand<T> parse(String sentence);
+public abstract class SentenceParser<T> {
+    public Operand<T> parse(String sentence){
+        String[] splitted = sentence.split(" ");
+        Operand<T> firstOperand = operandFromValue(fromString(splitted[0]));
+        Operand<T> lastOperand = firstOperand;
+        Operator<T> lastOperator = operatorFromSign(Operator.Sign.fromSignStr(splitted[1]));
+        boolean isOperand = true;
+        connect(lastOperand, lastOperator);
+        for(int i=2;i<splitted.length;i++){
+            String s = splitted[i];
+            if(isOperand){
+                lastOperand = operandFromValue(fromString(s));
+                connect(lastOperator, lastOperand);
+                isOperand = !isOperand;
+                continue;
+            }
+            lastOperator = new Operator<>(s);
+            connect(lastOperand, lastOperator);
+            isOperand = !isOperand;
+        }
+        return firstOperand;
+    }
+
+    protected abstract T fromString(String str);
+    protected abstract Operand<T> operandFromValue(T value);
+    protected abstract Operator<T> operatorFromSign(Operator.Sign sign);
+
+    protected void connect(Operand<T> operand, Operator<T> operator){
+        operand.connectLatter(operator);
+        operator.connectFormer(operand);
+    }
+
+    protected void connect(Operator<T> operator, Operand<T> operand){
+        operator.connectLatter(operand);
+        operand.connectFormer(operator);
+    }
 }
--- src/main/java/integerCalculator/IntegerSentenceParser.java
+++ src/main/java/integerCalculator/IntegerSentenceParser.java
@@ -4,36 +4,24 @@ import calculator.Operand;
 import calculator.Operator;
 import calculator.SentenceParser;
 
-public class IntegerSentenceParser implements SentenceParser<Integer> {
+public class IntegerSentenceParser extends SentenceParser<Integer> {
     @Override
-    public Operand<Integer> parse(String sentence) {
-        String[] splitted = sentence.split(" ");
-        Operand<Integer> firstOperand = new IntegerOperand(Integer.parseInt(splitted[0]));
-        Operand<Integer> lastOperand = firstOperand;
-        Operator<Integer> lastOperator = new IntegerOperator(Operator.Sign.fromSignStr(splitted[1]));
-        connect(lastOperand, lastOperator);
-        boolean isOperand = true;
-        for (int i=2;i<splitted.length;i++) {
-            String s = splitted[i];
-            if(isOperand){
-                lastOperand = new IntegerOperand(Integer.parseInt(s));
-                connect(lastOperator, lastOperand);
-                isOperand = !isOperand;
-                continue;
-            }
-            lastOperator = new Operator<>(s);
-            connect(lastOperand, lastOperator);
-            isOperand = !isOperand;
+    protected Integer fromString(String str) {
+        try{
+            return Integer.parseInt(str);
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+            throw new IllegalStateException();
         }
-        return firstOperand;
     }
 
-    private void connect(Operand<Integer> operand, Operator<Integer> operator){
-        operand.connectLatter(operator);
-        operator.connectFormer(operand);
+    @Override
+    protected Operand<Integer> operandFromValue(Integer value) {
+        return new IntegerOperand(value);
     }
-    private void connect(Operator<Integer> operator, Operand<Integer> operand){
-        operator.connectLatter(operand);
-        operand.connectFormer(operator);
+
+    @Override
+    protected Operator<Integer> operatorFromSign(Operator.Sign sign) {
+        return new IntegerOperator(sign);
     }
 }
