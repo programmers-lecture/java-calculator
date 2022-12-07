345fbd8a343d0347b91174db2e3fe96cfa039dda
hong-kyungsik <h.ks.dpr@gmail.com>
2022-12-07 21:30:20 +0900

    Add: 연산자, 피연산자 클래스 추가

---
+++ src/main/java/calculator/Operand.java
@@ -0,0 +1,30 @@
+package calculator;
+
+import lombok.Getter;
+
+@Getter
+public abstract class Operand<T> {
+    protected T value;
+    protected Operator<T> former;
+    protected Operator<T> latter;
+
+    public Operand(T value){
+        this.value = value;
+    }
+
+    public Operand(T value, Operator<T> former, Operator<T> latter){
+        this.value = value;
+        this.former = former;
+        this.latter = latter;
+    }
+
+    public abstract Operand<T> calculate(Operator<T> operator, Operand<T> other);
+
+    public void connectFormer(Operator<T> former){
+        this.former = former;
+    }
+
+    public void connectLatter(Operator<T> latter){
+        this.latter = latter;
+    }
+}
---
+++ src/main/java/calculator/Operator.java
@@ -0,0 +1,47 @@
+package calculator;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Operator<T> {
+    @RequiredArgsConstructor
+    public enum Sign{
+        ADD("+"),
+        SUBTRACT("-"),
+        MULTIPLY("*"),
+        DIVIDE("/");
+
+        private final String sign;
+
+        public static Sign fromSignStr(String sign){
+            for (Sign value : Sign.values()) {
+                if(value.sign.equals(sign)) return value;
+            }
+            throw new IllegalArgumentException();
+        }
+
+        public boolean isPrimal(){
+            return this==MULTIPLY || this==DIVIDE;
+        }
+    }
+
+    private final Sign sign;
+    private Operand<T> former;
+    private Operand<T> latter;
+
+    public Operator(String signStr){
+        this.sign = Sign.fromSignStr(signStr);
+    }
+
+    public void connectFormer(Operand<T> former){
+        this.former = former;
+    }
+
+    public void connectLatter(Operand<T> latter){
+        this.latter = latter;
+    }
+
+
+}
