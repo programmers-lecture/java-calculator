d2fd16d7e05932a83738f3a4bbbcb90d32fa8ae2
insub2004 <insub2004@naver.com>
2022-12-07 14:10:56 +0900

    '20221207'

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class StringCalculator {
+
+	public static void main(String[] args) {
+
+        int n = 5;
+        while(n>0) {
+
+            Scanner sc = new Scanner(System.in);
+
+            String str = sc.nextLine().replaceAll(" ","");
+
+            char[] arr = str.toCharArray();
+
+            int result = Character.getNumericValue(arr[0]);
+            for(int i=1; i<arr.length-1; i+=2){
+                //if(Character.getNumericValue(arr[i])==-1){        // anyway odd will be operator
+                    switch(arr[i]){
+                        case '+':
+                            result = getPlus(result,arr[i+1]);
+                            break;
+                        case '-':
+                            result = getMinus(result,arr[i+1]);
+                            break;
+                        case '*':
+                            result = getMul(result,arr[i+1]);
+                            break;
+                        case '/':
+                            result = getDiv(result,arr[i+1]);
+                            break;
+                    }
+                //}
+            }
+            System.out.printf("°á°ú : %d\n\n", result);
+
+            n--;
+        }
+    }
+
+    private static int getDiv(int result, char c) {
+        return result / Character.getNumericValue(c);
+    }
+
+    private static int getMul(int result, char c) {
+        System.out.println(result + " " + Character.getNumericValue(c));
+        return result * Character.getNumericValue(c);
+    }
+
+    private static int getMinus(int result, char c) {
+        return result - Character.getNumericValue(c);
+    }
+
+    private static int getPlus(int result, char c) {
+        return result + Character.getNumericValue(c);
+    }
+}
