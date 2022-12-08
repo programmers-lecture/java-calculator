e1d5177ba9b4252d034f81593cd0974b84f45f56
Chani17 <cucute8878@naver.com>
2022-12-08 16:25:43 +0900

    [3기][김찬희] 문자열 계산기 구현

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Calculator {
+
+    public static int calculate(String[] formula) {
+
+        Stack<Integer> numbers = new Stack<>();         // 숫자 저장 배열
+        Stack<String> operator = new Stack<>();         // 사칙연산 부호 저장 배열
+
+        // 맨 처음 숫자 number stack에 저장
+        int result = Integer.parseInt(formula[0]);
+        numbers.push(result);
+
+        for(int i=1; i<formula.length; i+=2) {
+            if (formula[i].equals("*")) {           // 곱하기 연산이 나오면 저장하지 않고 먼저 계산
+                result = multiply(numbers.pop(), Integer.parseInt(formula[i+1]));
+                numbers.push(result);               // 계산 결과값 number stack에 저장
+            } else if (formula[i].equals("/")) {    // 나누기 연산도 마찬가지로 저장하지 않고 먼저 계산
+                result = divide(Integer.parseInt(formula[i-1]), Integer.parseInt(formula[i+1]));
+                numbers.push(result);                // 계산 결과값 number stack에 저장
+            } else {            // 더하기, 빼기 연산자가 나올 경우 일단 number stack, operator stack에 저장
+                numbers.push(Integer.parseInt(formula[i+1]));
+                operator.push(formula[i]);
+            }
+        }
+
+        // 우선적으로 계산해줘야하는 연산자들(곱하기, 나누기)를 제외한 stack에 남아있는 연산들(더하기, 빼기) 수행
+        for(int i=0; i<operator.size(); i++) {
+            if (operator.get(i).equals("+")) {
+                result = add(numbers.pop(), numbers.pop());
+                numbers.push(result);
+            } else {
+                result = subtract(numbers.pop(), numbers.pop());
+                numbers.push(result);
+            }
+        }
+        return result;
+    }
+
+    // 더하기 연산
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    // 빼기 연산
+    static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // 곱하기 연산
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // 나누기 연산
+    static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] formula = input.split(" ");
+        int result = calculate(formula);
+        System.out.println("결과 : " + result);
+    }
+
+}
