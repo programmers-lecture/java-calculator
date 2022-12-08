698af0f7508eb33be795695e32a32c48b758c20a
kssumin <201566@jnu.ac.kr>
2022-12-08 17:08:28 +0900

    refactor : equals로 비교하도록 변경

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,22 +7,22 @@ public class Calculator {
 
     private static final String NOT_NUMBER="숫자가 아닙니다.";
     private static final String NOT_FOUND_OPERATOR="존재하지 않는 사칙연산 입니다.";
-    public Calculator(String prevNumber, String operand, String operation){
-        this.prevNumber=validateNumber(prevNumber);
+    public Calculator(Integer prevNumber, String operand, String operation){
+        this.prevNumber=prevNumber;
         this.operand=validateNumber(operand);
         this.operator=validateOperator(operation);
     }
     private Integer validateNumber(String a){
         try{
             return Integer.parseInt(a);
-        }catch (IllegalStateException e){
+        }catch (NumberFormatException e){
             throw new IllegalArgumentException(NOT_NUMBER);
         }
     }
 
     private Operator validateOperator(String operation){
         return Arrays.stream(Operator.values())
-                .filter(x->x.getOperation().equals(operation))
+                .filter(x-> x.getOperation().equals(operation))
                 .findAny()
                 .orElseThrow(()->new IllegalArgumentException(NOT_FOUND_OPERATOR));
     }
