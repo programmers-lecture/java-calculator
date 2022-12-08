e6fbeb525443d50fb2ad6776a05596ade40e1bc1
kssumin <201566@jnu.ac.kr>
2022-12-08 17:17:25 +0900

    refactor : 2개 이상 연산 가능하도록 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,16 +3,22 @@ import view.InputView;
 import view.OutputView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
-    private static final int FIRST_NUMBER_INDEX=0;
-    private static final int SECONDE_NUMBER_INDEX=2;
-    private static final int OPERATION_INDEX=1;
     public static void main(String[] args) throws IOException {
         String input=InputView.inputTarget();
         List<String>inputList= StringParser.parseInput(input);
-        Calculator calculator=new Calculator(inputList.get(FIRST_NUMBER_INDEX),inputList.get(SECONDE_NUMBER_INDEX),inputList.get(OPERATION_INDEX));
-        OutputView.result(calculator.calculateNumber());
+        List<String>operandList=StringParser.parseOperand(inputList);
+        List<String>operatorList=StringParser.parseOperator(inputList);
+        List<Integer>resultList=new ArrayList<>();
+        resultList.add(0);
+        for(int index=0;index<operandList.size();index++){
+            Calculator calculator=new Calculator(resultList.get(index),operandList.get(index),operatorList.get(index));
+            resultList.add(calculator.calculateNumber());
+        }
+        OutputView.result(resultList.get(resultList.size()-1));
     }
 }
--- src/main/java/utils/StringParser.java
+++ src/main/java/utils/StringParser.java
@@ -1,10 +1,29 @@
 package utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StringParser {
-    public static List<String> parseInput(String input){
+    private static final String INIT_OPERAND="+";
+    public static List<String> parseInput(String input) {
         return Arrays.asList(input.split(" "));
+    }
+
+    public static List<String> parseOperand(List<String> parseInput) {
+        List<String> operandList = new ArrayList<>();
+        for (int i = 0; i < parseInput.size(); i = i + 2) {
+            operandList.add(parseInput.get(i));
+        }
+        return operandList;
+    }
+
+    public static List<String> parseOperator(List<String> parseInput) {
+        List<String> operatorList = new ArrayList<>();
+        for (int i = 1; i < parseInput.size(); i = i + 2) {
+            operatorList.add(INIT_OPERAND);
+            operatorList.add(parseInput.get(i));
+        }
+        return operatorList;
     }
 }
