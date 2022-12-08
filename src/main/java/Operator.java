a57e504a7e7a91dc2fca2193257947eee52ab01d
kssumin <201566@jnu.ac.kr>
2022-12-08 13:41:37 +0900

    style : 코드 리포매팅

--- src/main/java/Operator.java
+++ src/main/java/Operator.java
@@ -1,37 +1,40 @@
-public enum Operator{
-    PLUE("+"){
+public enum Operator {
+    PLUE("+") {
         @Override
         public Integer calculate(Integer a, Integer b) {
-            return a+b;
+            return a + b;
         }
     },
     MINUE("-") {
         @Override
         public Integer calculate(Integer a, Integer b) {
-            return a-b;
+            return a - b;
         }
     },
-    DIVIDE("/"){
+    DIVIDE("/") {
         @Override
         public Integer calculate(Integer a, Integer b) {
-            return a/b;
+            return a / b;
         }
     },
-    MULTIPLE("*"){
+    MULTIPLE("*") {
         @Override
         public Integer calculate(Integer a, Integer b) {
-            return a*b;
+            return a * b;
         }
     };
-    
+
     private String operation;
-    
-    Operator(String operation){
-        this.operation=operation;
+
+    Operator(String operation) {
+        this.operation = operation;
     }
+
     public abstract Integer calculate(Integer a, Integer b);
 
-    public String getOperation(){
+    public String getOperation() {
         return operation;
     }
+
+
 }
