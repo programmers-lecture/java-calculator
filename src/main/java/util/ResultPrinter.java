b5512fec04e8144bae73713cd5e8d2dffcbec6ab
hong-kyungsik <h.ks.dpr@gmail.com>
2022-12-10 18:33:33 +0900

    Refactor: 불필요한 제네릭 파라미터 제거

--- src/main/java/util/ResultPrinter.java
+++ src/main/java/util/ResultPrinter.java
@@ -2,7 +2,7 @@ package util;
 
 import calculator.expression.Operand;
 
-public class ResultPrinter<T> {
+public class ResultPrinter {
     public static <T> void print(Operand<T> operand){
         System.out.println("결과 : "+operand.getValue().toString());
     }
