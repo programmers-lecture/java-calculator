75d89f90465d3dba11c2613e5dc31c8ddc4cfe3b
cdj2073 <79151181+cdj2073@users.noreply.github.com>
2022-12-12 16:30:35 +0900

    feat: 연산 과정 추가

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -1,5 +1,6 @@
 package Calculator;
 
+import java.awt.desktop.OpenFilesEvent;
 import java.util.Scanner;
 
 public class Calculator {
@@ -8,8 +9,20 @@ public class Calculator {
         String expression = scan.nextLine();
 
         String[] values = expression.split(" ");
-
+        System.out.println(calculate(values));
         scan.close();
+    }
+
+    public static int calculate(String[] values) {
+        Operator operator;
+        int nextValue;
+        int result = Integer.parseInt(values[0]);
+        for (int i = 1; i < values.length; i += 2) {
+            nextValue = Integer.parseInt(values[i + 1]);
+            operator = Operator.findOperator(values[i]);
+            result = operator.operate(result, nextValue);
+        }
+        return result;
     }
 
 }
