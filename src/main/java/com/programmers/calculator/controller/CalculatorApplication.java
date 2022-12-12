d9dab08fb44b2b61d1c781588951eccaae1433d0
minkyeng <minnn.xyz@gmail.com>
2022-12-12 23:15:51 +0900

    style : 주석 제거

--- src/main/java/com/programmers/calculator/controller/CalculatorApplication.java
+++ src/main/java/com/programmers/calculator/controller/CalculatorApplication.java
@@ -6,13 +6,9 @@ import com.programmers.calculator.view.OutputView;
 
 public class CalculatorApplication {
     public void run() {
-//      연산식 입력
         String[] expressions = new InputView().input();
-//      연산식 계산
         Calculator calculator = new Calculator();
         int result = calculator.calculate(expressions);
-//      연산식 출력
         new OutputView().print(result);
-
     }
 }
