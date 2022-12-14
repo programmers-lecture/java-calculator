c6c26a2607cd050cf271ad441935efb1b2ce1a81
insub2004 <insub2004@naver.com>
2022-12-14 18:25:58 +0900

    expression변수 상수로 변경

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static String expression;
     public static void main(String[] args) {
 
         System.out.println("1회용 문자열 계산기(제한:연산자1개 피연산자2개)");
 
-        expression = inputMethod();
-        System.out.println(expression);
+       final String expression = inputMethod();
+
+
 
     }
 
