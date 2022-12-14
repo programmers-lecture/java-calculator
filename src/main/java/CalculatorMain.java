41a23c8a986c6ea95d8a2630815bd87eada938e1
s2s22 <sycho3713@hanmail.net>
2022-12-15 01:41:29 +0900

    계산기 완료

--- src/main/java/CalculatorMain.java
+++ src/main/java/CalculatorMain.java
@@ -1,82 +1,74 @@
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.Stack;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 
 public class CalculatorMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //1+2*3-4/5
         System.out.println("사칙연산 식을 입력해주세요.");
 
         Scanner sc = new Scanner(System.in);
-        String cal = sc.next();
+        String cal = sc.nextLine();
 
-//30-3*30+30+30*300/30*30  곱하기 곱하기 나누기 곱하기
-        int reuslt = 0;
-        //우선순위
-        while (true) {
-            if(cal.indexOf("*") ==-1 && cal.indexOf("/") ==-1 ) break;
-            int multiIdx = cal.indexOf("*") == -1 ? cal.length() : cal.indexOf("*");
-            int divIdx = cal.indexOf("/") == -1 ? cal.length() : cal.indexOf("/");
-            if(multiIdx < divIdx) {
-                int idx = cal.indexOf("*");//현재 연산자 인덱스
-                int before = beforeFindIdx(cal, idx);
-                int after = afterFindIdx(cal, idx);
-                String str = cal.substring(before +1, idx) + "*" + cal.substring(idx+1, after);//연산식
 
-                reuslt = Integer.parseInt(cal.substring(before +1, idx)) * Integer.parseInt(cal.substring(idx+1, after));
-                cal = cal.replace(str, String.valueOf(reuslt));
+
+        Stack opratorSt = new Stack(); //  연산자 스택
+        String postfix = "";
+        for(char c : cal.replaceAll(" ", "").toCharArray()) {
+            Stream<operator> stream = Arrays.stream(operator.values());
+
+            if(Arrays.stream(operator.values()).anyMatch( i -> i.getOp().equals(String.valueOf(c)))) { //연산자면
+                //스택 top < curr -> push
+                if(opratorSt.isEmpty() || operator.ADD.comparePriority((String) opratorSt.peek(), c+"")) {
+                    opratorSt.push(c + "");
+                } else {
+                    postfix += opratorSt.pop();
+                    while (!opratorSt.isEmpty() && !operator.ADD.comparePriority((String) opratorSt.peek(), c+"")) {
+                        postfix += opratorSt.pop();
+                    }
+                    opratorSt.push(c + "");
+                }
+                //스책 top >= curr -> 게속 스트링에 ++
+            } else postfix += c + "";
+        }
+        while (!opratorSt.isEmpty()) postfix += opratorSt.pop();
+        int calculator = calculator(postfix);
+
+        System.out.println("결과 값 " + calculator);
+    }
+
+    //후순위 순서대로 계산
+    public static int calculator(String postfix) {
+        Stack st = new Stack();
+        Arrays.stream(postfix.split("")).forEach(expression -> {
+            if(Character.isDigit(expression.charAt(0))) {
+                st.push(expression);
+            }else {
+
+                st.push(formula(Integer.parseInt(st.pop().toString()), Integer.parseInt(st.pop().toString()), expression));
             }
-            else if(divIdx < multiIdx) {
-                int idx = cal.indexOf("/");//현재 연산자 인덱스
-                int before = beforeFindIdx(cal, idx);
-                int after = afterFindIdx(cal, idx);
-                String str = cal.substring(before +1, idx) + "/" + cal.substring(idx+1, after);//연산식
-
-                reuslt = Integer.parseInt(cal.substring(before +1, idx)) / Integer.parseInt(cal.substring(idx+1, after));
-                cal = cal.replace(str, String.valueOf(reuslt));
-            }
+        });
 
 
-        }
-        System.out.println("최종결과 : " + cal);
-//60-30+40
-        //우선순위 후 차례대로
-
-
+        return Integer.parseInt(st.pop().toString());
     }
 
-    public static int beforeFindIdx(String str, int idx) {
-        int before = -1;
-
-        //전인덱스
-        if(str.lastIndexOf("+", idx-1) > 0) {
-            before = str.lastIndexOf("+", idx - 1);
+    //연산식 계산
+    public static int formula(int first , int second, String op) {
+        switch (op) {
+            case "+" :
+                return second + first;
+            case "-":
+                return second - first;
+            case "*":
+                return second * first;
+            case "/":
+                return second / first;
         }
-        if (str.lastIndexOf("-", idx-1) > 0) {
-            before = str.lastIndexOf("-", idx-1) > before ? str.lastIndexOf("-", idx-1): before;
-        }
-        if (str.lastIndexOf("*", idx-1) > 0) {
-            before = str.lastIndexOf("*", idx-1) > before ? str.lastIndexOf("*", idx-1): before;
-        }
-        if (str.lastIndexOf("/", idx-1) > 0) {
-            before = str.lastIndexOf("/", idx-1) > before ? str.lastIndexOf("/", idx-1): before;
-        }
-        return before;
+        return 0;
     }
 
-    public static int afterFindIdx(String str, int idx) {
-        int after = str.length();
-        if(str.indexOf("+", idx+1) > 0) {
-            after = str.indexOf("+", idx + 1);
-        }
-        if (str.indexOf("-", idx+1) > 0) {
-            after = str.indexOf("-", idx+1) < after ? str.indexOf("-", idx+1): after;
-        }
-        if (str.indexOf("*", idx+1) > 0) {
-            after = str.indexOf("*", idx+1) < after ? str.indexOf("*", idx+1): after;
-        }
-        if (str.indexOf("/", idx+1) > 0) {
-            after = str.indexOf("/", idx+1) < after ? str.indexOf("/", idx+1): after;
-        }
-        return after;
-    }
+
 }
